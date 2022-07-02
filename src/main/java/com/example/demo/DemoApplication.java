986d019c0aa6264117bae0a5871a4259be1693f6
marco.hazan <marcoh427@gmail.com>
2022-07-02 12:18:21 +0200

    aa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		System.out.println("ciao");
+	public static void main(String[] args) {	
+		System.out.println("OK");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
